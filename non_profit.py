56aa670a6518fae7f766e44aba580d0be04c1bb6
Kaelan <engholdt8911@uwlax.edu>
2020-01-29 11:32:42 -0600

    initial commit

---
+++ non_profit.py
@@ -0,0 +1,20 @@
+'''
+Description
+ 
+Authors: Alex, Braden, Kaelan
+Date: 1/29/2020
+
+'''
+
+
+# imports
+
+
+def main():
+    # TODO finish this
+    pass
+
+
+if __name__ == "__main__":
+    # call to main program
+    main()
