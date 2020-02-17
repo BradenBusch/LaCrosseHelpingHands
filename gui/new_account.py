06516dadc2c8304debfdfedf117023f72c151ab8
BradenBusch <bradenbusch01@gmail.com>
2020-02-17 11:41:12 -0600

    Changed up NewAccount, add functionality to support Admin authorization.

--- gui/new_account.py
+++ gui/new_account.py
@@ -13,15 +13,17 @@ class NewAccount(QWidget):
         self.email_edit = QLineEdit()
         self.password_edit = QLineEdit()
         self.confirm_password_edit = QLineEdit()
+        self.admin_code_box = QLineEdit()
+        self.admin_code_box.hide()
         self.fields = [self.username_edit, self.email_edit, self.password_edit, self.confirm_password_edit]
-        self.radio_btn_names = ['Volunteer', 'Staff', 'Admin']
-
+        self.radio_btns = []
+        self.vbox = QVBoxLayout()
         self.draw()
 
     # Build Create Account window
     # TODO add checkboxes for choosing an account type (admin needs extra code or something idk)
     def draw(self):
-        self.setFixedSize(300, 300)
+        # self.setFixedSize(300, 300)
         self.setWindowTitle("Make a new account")
 
         self.password_edit.setEchoMode(QLineEdit.Password)
@@ -30,13 +32,18 @@ class NewAccount(QWidget):
         confirm_btn = QPushButton()
         cancel_btn = QPushButton()
         confirm_btn.clicked.connect(self.verify_fields)
-        cancel_btn.clicked.connect(self.go_back)  # TODO nah we gotta go back
+        cancel_btn.clicked.connect(self.go_back)
 
         volunteer_radio = QRadioButton('Volunteer')
         volunteer_radio.setChecked(True)
+        volunteer_radio.toggled.connect(self.hide_admin)
         staff_radio = QRadioButton('Staff')
+        staff_radio.toggled.connect(self.hide_admin)
         admin_radio = QRadioButton('Admin')
-
+        admin_radio.toggled.connect(self.admin_click)
+        self.radio_btns.append(volunteer_radio)
+        self.radio_btns.append(staff_radio)
+        self.radio_btns.append(admin_radio)
         confirm_btn.setText("Confirm")
         cancel_btn.setText("Cancel")
         self.username_edit.setPlaceholderText("Username (At least 8 Characters)")
@@ -44,20 +51,29 @@ class NewAccount(QWidget):
         self.password_edit.setPlaceholderText("Password (At least 8 Characters)")
         self.confirm_password_edit.setPlaceholderText("Confirm Password")
         self.confirm_password_edit.returnPressed.connect(self.verify_fields)
+        self.admin_code_box.setPlaceholderText("Enter the Admin Password")
 
-        vbox = QVBoxLayout()
-        hbox = QHBoxLayout()
-        vbox.addWidget(self.username_edit)
-        vbox.addWidget(self.email_edit)
-        vbox.addWidget(self.password_edit)
-        vbox.addWidget(self.confirm_password_edit)
-        vbox.addWidget(volunteer_radio)
-        vbox.addWidget(staff_radio)
-        vbox.addWidget(admin_radio)
-        hbox.addWidget(cancel_btn)
-        hbox.addWidget(confirm_btn)
-        vbox.addLayout(hbox)
-        self.setLayout(vbox)
+        hbox1 = QHBoxLayout()
+        hbox2 = QHBoxLayout()
+        self.vbox.addWidget(self.username_edit)
+        self.vbox.addWidget(self.email_edit)
+        self.vbox.addWidget(self.password_edit)
+        self.vbox.addWidget(self.confirm_password_edit)
+        self.vbox.addWidget(self.admin_code_box)
+        for btn in self.radio_btns:
+            hbox1.addWidget(btn)
+
+        hbox2.addWidget(cancel_btn)
+        hbox2.addWidget(confirm_btn)
+        self.vbox.addLayout(hbox1)
+        self.vbox.addLayout(hbox2)
+        self.setLayout(self.vbox)
+
+    def admin_click(self):
+        self.admin_code_box.show()
+
+    def hide_admin(self):
+        self.admin_code_box.hide()
 
     # Verifies that the Username/Email aren't in use, that the passwords match, and the passwords are 8 characters long
     def verify_fields(self):
@@ -92,7 +108,8 @@ class NewAccount(QWidget):
         # Move to the next GUI, all checks passed
         else:
             stored_password = hash_password(password)
-            self.store_user(username, email, stored_password)  # TODO account type
+            account_type = self.get_account_type()
+            self.store_user(username, email, stored_password, account_type)
 
     def clear_fields(self):
         for f in self.fields:
@@ -103,14 +120,19 @@ class NewAccount(QWidget):
         self.parent().parent().set_page(0)
         pass
 
+    def get_account_type(self):
+        for btn in self.radio_btns:
+            if btn.isChecked():
+                return btn.text()  # TODO When admin, show admin code
+
     # Store the new users information in the database
-    # TODO make passwords encrypted
     def store_user(self, username, email, password, account_type=None):
         user_id = self.generate_id()
         hashed_pass = hash_password(password)
         new_user = User(user_id=user_id, username=username, password=hashed_pass, email=email, account_type=account_type)
         new_user.save()
 
+    # TODO check id's, if this is even needed anyway
     # Generate a unique ID for each user in the database
     def generate_id(self):
         # return uuid.uuid4().hex[:4]
